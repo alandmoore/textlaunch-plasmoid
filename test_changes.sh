8e2e01201d6a2b90f6f1467993e888d2fd9b47bb
Alan D Moore <me@alandmoore.com>
2010-11-15 16:44:31 -0600

    fix to test script to exlcude git tree

--- test_changes.sh
+++ test_changes.sh
@@ -1,6 +1,6 @@
 PLASMOID_NAME=textlaunch
 
-zip -r ../$PLASMOID_NAME.plasmoid .
+zip -r ../$PLASMOID_NAME.plasmoid . -x '*.git/*'
 plasmapkg -r $PLASMOID_NAME
 plasmapkg -i ../$PLASMOID_NAME.plasmoid
 
